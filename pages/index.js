b45739f696c2351ae2a1fa29d538b25fb42b5b26
Carl-M. Goersch <goersch@madways.de>
2023-05-09 14:11:59 +0200

    button works

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,23 @@ import FriendsPagPreview from "@/components/FriendPagePreview";
 import Header from "@/components/Header";
 import Link from "next/link";
 import { StyledLink } from "@/components/Link/Link.Styled";
+import styled from "styled-components";
 
+const StyledIndex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  flex-wrap: wrap;
+  text-align: center;
+`;
 export default function HomePage() {
   return (
     <div>
-      <Header />
-      <RandomFunction data={data} />
+      <StyledIndex>
+        <Header />
+        <RandomFunction data={data} />
+      </StyledIndex>
       <StyledLink href={`/contacts`}> Contacts</StyledLink>
     </div>
   );
