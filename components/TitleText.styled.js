e330d3fec70d69ee3f8318dca13d0591cb9fc22f
Carl-M. Goersch <goersch@madways.de>
2023-05-09 14:52:44 +0200

    titel text

---
+++ components/TitleText.styled.js
@@ -0,0 +1,8 @@
+// import styled from "styled-components";
+
+// export const StyledTitleText = styled.h1`
+//   color: white;
+//   text-align: center;
+//   font-size: 1rem;
+//   margin: 1.4rem;
+// `;
