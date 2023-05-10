9ab3137198e3fbc22b1f9db2e9ea269f73c04119
Carl-M. Goersch <goersch@madways.de>
2023-05-10 09:21:43 +0200

    some styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import data from "@/data.json";
 import RandomFunction from "@/components/RandomFunction";
-import FriendsPage from "@/components/FriendPages";
-import FriendsPagPreview from "@/components/FriendPagePreview";
 import Header from "@/components/Header";
-import Link from "next/link";
 import { StyledLink } from "@/components/Link/Link.Styled";
 
 import styled from "styled-components";
