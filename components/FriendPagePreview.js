c7db7ce7f7c0470324fc6d012efc10f343629d8a
Carl-M. Goersch <goersch@madways.de>
2023-05-16 19:51:19 +0200

    file safed

--- components/FriendPagePreview.js
+++ components/FriendPagePreview.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styled from "styled-components";
 import { StyledButton } from "./Button/Button.Styled";
-import { StyledList } from "./List/1List.Styled";
+import { StyledList } from "./List/List.Styled";
 import { useState } from "react";
 
 const StyledDiv = styled.div`
